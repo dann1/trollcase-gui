217f0acd840e545b637494895e2eeccafdae6398
Daniel Clavijo Coca <dclavijo@opennebula.io>
2023-11-12 20:56:31 -0600

    Clipboard

--- src/main.rs
+++ src/main.rs
@@ -5,33 +5,49 @@ fn main() {
     let app = app::App::default().with_scheme(app::Scheme::Gtk);
 
     // Window properties
-    let window = Window::new(100, 100, 400, 150, "TrollCase");
+    let window = Window::new(100, 100, 400, 150, button_title("trollcase", casing::randomize));
 
     // Setup text input
-    let mut input = Input::new(60, 20, 270, 25, "");
+    let input = Input::new(60, 20, 270, 25, "");
 
-    // Setup buttons
-    let mut btn_randomize = Button::new(50, 100, 80, 40, "Randomize");
-    let mut btn_alternate = Button::new(270, 100, 80, 40, "Alternate");
+    // Create buttons
+    let mut btn_randomize = Button::new(50, 100, 90, 40, button_title("randomize", casing::randomize));
+    let mut btn_alternate = Button::new(270, 100, 90, 40, button_title("alternate", casing::alternate));
+    let mut btn_copy = Button::new(350, 23, 35, 20, "copy");
 
-    // Functionality
-    fn connect_button_with_caser<F>(button: &mut Button, input: &mut Input, caser: F)
-    where
-        F: Fn(&str) -> String + 'static,
-    {
-        let mut input = input.clone();
-        button.set_callback(move |_| {
-            let text = input.value();
-            let result = caser(&text);
-            input.set_value(&result);
-        });
-    }
+    // Casers
+    connect_button_with_caser(&mut btn_randomize, &input, casing::randomize);
+    connect_button_with_caser(&mut btn_alternate, &input, casing::alternate);
 
-    connect_button_with_caser(&mut btn_randomize, &mut input, casing::randomize);
-    connect_button_with_caser(&mut btn_alternate, &mut input, casing::alternate);
-
-    // TODO: Copy to clipboard button
+    // Clipboard
+    btn_copy.set_callback({
+        let input = input.clone();
+        move |_| {
+            app::copy(&input.value())
+        }
+    });
 
     window.center_screen().show();
     app.run().unwrap();
 }
+
+fn button_title<F>(title: &str, caser: F) -> &'static str
+where
+    F: Fn(&str) -> String,
+{
+    return Box::leak(caser(title).into_boxed_str());
+}
+
+
+fn connect_button_with_caser<F>(button: &mut Button, input: &Input, caser: F)
+where
+    F: Fn(&str) -> String + 'static,
+{
+    let mut input = input.clone();
+    button.set_callback(move |_| {
+        let text = input.value();
+        let result = caser(&text);
+        input.set_value(&result);
+    });
+}
+
